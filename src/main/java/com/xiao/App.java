0ede8571964f01968992434287adaefeb609fc6d
sunjinwei4 <sunjinwei@jd.com>
2022-09-26 17:23:47 +0800

    反射整理

--- src/main/java/com/xiao/App.java
+++ src/main/java/com/xiao/App.java
@@ -1,5 +1,13 @@
 package com.xiao;
 
+import cn.hutool.core.exceptions.UtilException;
+import cn.hutool.json.JSONUtil;
+import lombok.Data;
+import cn.hutool.core.util.ReflectUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +17,55 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+}
+
+
+@Data
+class PP{
+    private String key;
+    private String value;
+
+    public PP(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+@Data
+class QQ{
+    private String name1;
+//    private String name2;
+    private String name3;
+}
+
+
+class Test{
+    public static void main(String[] args) {
+        PP p1 = new PP("name1", "张三1");
+        PP p2 = new PP("name2", "张三2");
+        PP p3 = new PP("name3", "张三3");
+
+        List<PP> list = new ArrayList<>();
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+
+        list.stream().forEach( p->{
+            String key = p.getKey();
+            String value = p.getValue();
+            System.out.println(JSONUtil.toJsonStr(p));
+
+
+            QQ q = new QQ();
+            try {
+                ReflectUtil.setFieldValue(q, key, value);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(JSONUtil.toJsonStr(q));
+            System.out.println("===========");
+        });
+
     }
 }
